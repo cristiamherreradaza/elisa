9cfde586e34120312121013422ce380b5dd6c2b3
cristiam <cristiamherrera@gmail.com>
2021-02-26 15:45:24 -0400

    Se adiciono la function auth

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PanelController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function inicio()
     {
         // echo "holas";
