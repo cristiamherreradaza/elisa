f374cef0a15c75ed9f1a7dd0bb3492407b2a906f
cristiam <cristiamherrera@gmail.com>
2021-03-02 17:35:37 -0400

    Se adicionaron los modelos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,6 @@ class UserController extends Controller
 
     public function nuevo()
     {
-    	
+    			
     }
 }
---
+++ database/migrations/2021_03_01_223745_add_campos_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCamposToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('almacen_id')->nullable()->after('id');
+            $table->foreign('almacen_id')->references('id')->on('almacenes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            
+        });
+    }
+}
