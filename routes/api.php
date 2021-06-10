56c1fe634e6fffbf4d0a402220493ca2d2aabfff
Cristiam Herrera Daza <47664718+cristiamherreradaza@users.noreply.github.com>
2021-06-10 16:14:24 -0400

    Se adiciono un comentario

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+// contrtoladores para la api 
 
 Route::get('Publicaciones/listado', 'api\PublicacionesController@listado');
 Route::post('Dispositivos/registra', 'api\DispositivosController@registra');
