41e0341377c02f8f806403ebfc5fef3506c4bd36
Cristiam Herrera <cristiamherrera@gmail.com>
2021-12-01 20:28:20 -0400

    Se cambio el idioma de la ubicacion

--- resources/views/localizacion/mapa.blade.php
+++ resources/views/localizacion/mapa.blade.php
@@ -41,7 +41,7 @@ function initMap() {
 
   const locationButton = document.createElement("button");
 
-  locationButton.textContent = "Pan to Current Location";
+  locationButton.textContent = "Llevame a mi ubicacion";
   locationButton.classList.add("custom-map-control-button");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 
