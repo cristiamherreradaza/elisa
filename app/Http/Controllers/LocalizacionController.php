2ad0c4c871c2d5a73b3df847a597172725e614e1
Cristiam Herrera <cristiamherrera@gmail.com>
2022-02-08 18:54:53 -0400

    Se valido el acceso al mapa

--- app/Http/Controllers/LocalizacionController.php
+++ app/Http/Controllers/LocalizacionController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LocalizacionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function mapa(){
 
         $localizaciones = Localizacion::all();
