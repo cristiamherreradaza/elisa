63c9589a9477ee97dfab5f63f0e84d3993113594
Cristiam Herrera <cristiamherrera@gmail.com>
2022-02-09 11:37:10 -0400

    Se quito los permisos para la api

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -11,14 +11,6 @@ use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    //
-    public function __construct()
-    {
-        // $this->middleware('auth');
-        $this->middleware('auth');
-
-    }
-
 
     public function familiares(Request $request)
     {
